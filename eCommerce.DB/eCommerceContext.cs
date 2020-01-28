3d4b7ace01433c40c340c82b71562e919fc668db
abdulrehmantariq <boyrocker203@gmail.com>
2020-01-29 01:06:53 +0500

    Added Business Category product,category and create database

---
+++ eCommerce.DB/eCommerceContext.cs
@@ -0,0 +1,20 @@
+﻿using eCommerce.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.DB
+{
+    class eCommerceContext : DbContext 
+    {
+        public eCommerceContext() : base ("DefaultConnection")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ eCommerce.Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Entities
+{
+    public class Product:BaseEntities
+    {
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+    }
+}
